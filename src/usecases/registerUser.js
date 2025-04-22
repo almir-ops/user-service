7be30285e91d97825effa955dd8a76486cb66265
Almir Libório Batista Junior <almir_jesus2@hotmail.com>
2025-04-22 13:24:32 -0300

    fix name

--- src/usecases/registerUser.js
+++ src/usecases/registerUser.js
@@ -1,13 +1,18 @@
-// src/usecases/registerUser.js
-
+const bcrypt = require('bcryptjs');
 const SequelizeUserRepository = require('../infra/repositories/SequelizeUserRepository'); 
 
 async function registerUser(userData) {
   // Verificar se o email já está em uso
   const existingUser = await SequelizeUserRepository.findByEmail(userData.email);
   if (existingUser) {
-    throw new Error('Email ja está em uso');
+    throw new Error('Email já está em uso');
   }
+
+  // Criptografar a senha
+  const hashedPassword = await bcrypt.hash(userData.password, 10); // 10 = salt rounds
+
+  // Substituir a senha original pela criptografada
+  userData.password = hashedPassword;
 
   // Criar o usuário no banco de dados
   const user = await SequelizeUserRepository.create(userData);
